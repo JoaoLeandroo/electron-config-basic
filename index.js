9d32cb2a2abb14824ad1eb830dd3d696db3eeee0
joaoleandro <new.joaoleandro@gmail.com>
2023-10-15 14:11:41 -0300

    initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const electron = require('electron')
+
+const url = require('url')
+
+const path = require('path')
+
+const { Menu } = require('electron')
+
+const{ app, BrowserWindow } = electron
+
+let mainWindow 
+let addWindow
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({})
+
+
+    mainWindow.loadURL(url.format(
+        {
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file',
+            slashes: true
+        }
+    ))
+})
